8d01941fac305c628dc60c4547f150c1d8871f1d
phlippieb <phlippie.bosman@gmail.com>
2024-07-08 20:25:44 +0200

    Change site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://phlippieb.github.io',
+  site: 'https://phlippieb.dev',
   markdown: {
     shikiConfig: {
       theme: 'nord',
