63f97b89b482c0f68a418686a2c591b7861b388a
phlippieb <phlippie.bosman@gmail.com>
2024-07-08 15:38:59 +0200

    Configure site for phlippieb.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://phlippieb.github.io',
   markdown: {
     shikiConfig: {
       theme: 'nord',
